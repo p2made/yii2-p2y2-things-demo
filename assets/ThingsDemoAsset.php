a6ea9f3688aa5eea2384ff22bc91e8e1a1c28425
Pedro Plowman <pedrofp@me.com>
2015-06-03 22:16:58 +1000

    v0.9.3

--- assets/ThingsDemoAsset.php
+++ assets/ThingsDemoAsset.php
@@ -12,7 +12,7 @@ namespace p2made\demo\Things\assets;
 
 class ThingsDemoAsset extends \yii\web\AssetBundle
 {
-	public $sourcePath = '@vendor/p2made/yii2-asset-demo-theme/assets/lib';
+	public $sourcePath = '@vendor/p2made/yii2-p2y2-things-demo/assets/lib';
 
 	public $css = [
 		'css/asset-demo.css',
